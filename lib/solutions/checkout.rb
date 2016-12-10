22c6217fe63ebca70003671629eac052d1272303
Douglas Waugh <douglas.waugh.e3@gmail.com>
2016-12-10 10:30:26 +0000

    Refactor price of A in to hashtable

--- lib/solutions/checkout.rb
+++ lib/solutions/checkout.rb
@@ -1,10 +1,16 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  def initialize
+    @prices = {
+      :A => 50 
+    }
+  end
+
   def checkout(skus)
     return 0 if skus == "";
 
-    return 50
+    return @prices[skus.to_sym]
   end
 
 end
